c921a3a4fea4949f07e3d7e24e18cfb1d02957b0
dinikop <zarazablin@gmail.com>
2018-11-18 18:33:26 +0300

    implements clear at SortedArrayStorage

--- src/storage/AbstractArrayStorage.java
+++ src/storage/AbstractArrayStorage.java
@@ -10,6 +10,11 @@ public abstract class AbstractArrayStorage implements Storage {
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
 
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
     public int size() {
         return size;
     }
