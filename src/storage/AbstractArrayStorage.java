4364ed1098e28103aff29cac40f6c26266cd8e75
dinikop <zarazablin@gmail.com>
2018-11-18 18:10:54 +0300

    implements save at SortedArrayStorage

--- src/storage/AbstractArrayStorage.java
+++ src/storage/AbstractArrayStorage.java
@@ -3,7 +3,7 @@ package storage;
 import model.Resume;
 
 public abstract class AbstractArrayStorage implements Storage {
-    protected static final int STORAGE_LIMIT = 10000;
+    protected static final int STORAGE_LIMIT = 10_000;
 
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
--- src/storage/SortedArrayStorage.java
+++ src/storage/SortedArrayStorage.java
@@ -17,7 +17,19 @@ public class SortedArrayStorage extends AbstractArrayStorage{
 
     @Override
     public void save(Resume r) {
-
+        if (size >= STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
+        } else {
+            int index = getIndex(r.getUuid());
+            if (index >= 0) {
+                System.out.println("Resume " + r.getUuid() + " already exist");
+            } else {
+                index = Math.abs(index) - 1;
+                if (size - index >= 0) System.arraycopy(storage, index, storage, index + 1, size - index);
+                storage[index] = r;
+                size++;
+            }
+        }
     }
 
     @Override
