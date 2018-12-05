970d4ed8faf1632d85857cc721df9d98c690ef7f
dinikop <zarazablin@gmail.com>
2018-12-05 21:21:07 +0300

    implements ListStorage

--- src/storage/ListStorage.java
+++ src/storage/ListStorage.java
@@ -1,5 +1,51 @@
 package storage;
 
-public class ListStorage {
+import model.Resume;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListStorage extends AbstractStorage {
+
+    protected List<Resume> storage = new ArrayList<>();
+
+    @Override
+    public Resume[] getAll() {
+        return storage.toArray(new Resume[size]);
+    }
+
+    @Override
+    protected void updateSize() {
+        size = storage.size();
+    }
+
+    @Override
+    protected boolean isContained(String uuid) {
+        return storage.contains(new Resume(uuid));
+    }
+
+    @Override
+    protected void insertResume(Resume r) {
+        storage.add(r);
+    }
+
+    @Override
+    protected Resume retrieve(String uuid) {
+        return storage.get(storage.indexOf(new Resume(uuid)));
+    }
+
+    @Override
+    protected void remove(String uuid) {
+        storage.remove(new Resume(uuid));
+    }
+
+    @Override
+    protected void replace(Resume r) {
+        storage.add(r);
+    }
+
+    @Override
+    protected void clean() {
+        storage.clear();
+    }
 }
