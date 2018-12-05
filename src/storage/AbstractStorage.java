0f8aa2bd035173c96294241177ed4c24a515f46f
dinikop <zarazablin@gmail.com>
2018-12-05 20:41:06 +0300

    implements methods in AbstractStorage

--- src/storage/AbstractStorage.java
+++ src/storage/AbstractStorage.java
@@ -1,6 +1,7 @@
 package storage;
 
 import exception.ExistStorageException;
+import exception.NotExistStorageException;
 import exception.StorageException;
 import model.Resume;
 
@@ -12,12 +13,17 @@ public abstract class AbstractStorage implements Storage {
 
     @Override
     public void clear() {
-
+        clean();
+        updateSize();
     }
 
     @Override
     public void update(Resume r) {
-
+        if (!isContained(r.getUuid())) {
+            throw new NotExistStorageException(r.getUuid());
+        } else {
+            replace(r);
+        }
     }
 
     @Override
@@ -25,7 +31,7 @@ public abstract class AbstractStorage implements Storage {
         if (size >= STORAGE_LIMIT) {
             throw new StorageException("Storage overflow", r.getUuid());
         } else {
-            if (containsResume(r)) {
+            if (isContained(r.getUuid())) {
                 throw new ExistStorageException(r.getUuid());
             } else {
                 insertResume(r);
@@ -36,25 +42,40 @@ public abstract class AbstractStorage implements Storage {
 
     @Override
     public Resume get(String uuid) {
-        return null;
+        if (!isContained(uuid)) {
+            throw new NotExistStorageException(uuid);
+        }
+        return retrieve(uuid);
     }
 
     @Override
     public void delete(String uuid) {
-
-    }
-
-    @Override
-    public Resume[] getAll() {
-        return new Resume[0];
+        if (!isContained(uuid)) {
+            throw new NotExistStorageException(uuid);
+        } else {
+            remove(uuid);
+            updateSize();
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
+    public abstract Resume[] getAll();
+
     protected abstract void updateSize();
-    protected abstract boolean containsResume(Resume r);
+
+    protected abstract boolean isContained(String uuid);
+
     protected abstract void insertResume(Resume r);
+
+    protected abstract Resume retrieve(String uuid);
+
+    protected abstract void remove(String uuid);
+
+    protected abstract void replace(Resume r);
+
+    protected abstract void clean();
 }
