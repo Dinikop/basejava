d4b20303a69275b7ef04b60c5abb3522a68432a9
dinikop <zarazablin@gmail.com>
2018-12-16 13:51:06 +0300

    HW6

---
+++ test/AllStorageTest.java
@@ -0,0 +1,14 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import storage.*;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ArrayStorageTest.class,
+        SortedArrayStorageTest.class,
+        ListStorageTest.class,
+        MapUuidStorageTest.class,
+        MapResumeStorageTest.class
+})
+public class AllStorageTest {
+}
